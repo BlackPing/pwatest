07a15aab5ff978972a047f581cf03513b431e70f
BlackPing <bug_ping@naver.com>
2020-05-22 14:42:56 +0900

    test

--- Server/index.js
+++ Server/index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const fs = require('fs');
 const app = express();
 
-app.use('/', express.static('./build'));
+const static_path = path.join(__dirname, '/../build/static');
+app.use('/static', express.static(static_path));
 
 app.listen(7800, '192.168.219.105', () => {
 	console.log('server on');
